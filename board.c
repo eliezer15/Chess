0d7edde64b2d0acd0bb0c088c60229552aec1130
Eliezer Encarnacion <encarnae@live.unc.edu>
2013-08-14 00:52:24 -0400

    Added printBoard function

--- board.c
+++ board.c
@@ -3,16 +3,71 @@
 #include "board.h"
 #include <stdio.h>
 #define LOWERCASE 97
+#define CASEOFFSET 32
+/* Piece representation legend:
+* R: Rook
+* N:Knight
+* B: Bishop
+* Q: Queen
+* K: King
+* P: Pawn
+*
+* For simplicity purposes
+* the black pieces will use uppercase letters
+* 
+*/
 
 void printBoard(struct Space* board[][8]);
 
 void printBoard(struct Space* board[][8]) {
     
     int i,j;
+    char piece;
     
     //First print the letter columns
-    for (i = 0; i < 8; i++) 
-        printf("%c  ", LOWERCASE + i);
-    
+    printf("      "); //print 6 spaces for style
+    for (i = 0; i < 8; i++) { 
+        printf("%c     ", LOWERCASE + i);
+    }
+
+    for (i = 0; i < 8; i++) {
+        
+        printf("\n   -------------------------------------------------\n");
+        printf("%d",i+1);
+
+        for (j = 0; j < 8; j++) {
+            //See if there is a piece on the space to print
+            if (board[i][j]->piece) {
+                switch(board[i][j]->piece->piece_type) 
+                {
+                    case(pawn):   piece = 'P';
+                                  break;
+                    case(rook):   piece = 'R';
+                                  break;
+                    case(knight): piece = 'N';
+                                  break;
+                    case(bishop): piece = 'B';
+                                  break;
+                    case(queen):  piece = 'Q';
+                                  break;
+                    case(king):   piece = 'K';
+                                  break;
+                    default:      piece = ' ';
+                                  break;
+                }
+
+                if (board[i][j]->piece->isWhite)
+                    piece += CASEOFFSET; //turn lowercase
+            }
+            
+            else piece = ' ';
+        
+            printf("  |  ");
+            printf("%c", piece);
+
+        } 
+        printf("  |  ");
+   }
+   printf("\n   ------------------------------------------------\n\n");
 }
     
--- testPrintBoard.c
+++ testPrintBoard.c
@@ -1,10 +1,56 @@
 #include "board.h"
 #include "piece.h"
 #include "space.h"
-
+#include <stdbool.h>
+#include <stdlib.h>
+/* Notes
+* I allocate space in the array for 10 major pieces(except on King) because it is possible
+* to have as many as 10, considering all 8 pawns promote to the given
+* piece. Very unlikely, but technically possible.
+*/
 int main() {
     
     struct Space* board[8][8];
+    
+    struct Piece* whitePawns[8];
+    struct Piece* blackPawns[8];
+
+    struct Piece* whiteRooks[10];
+    struct Piece* blackRooks[10];
+
+    struct Piece* whiteBishops[10];
+    struct Piece* blackBishops[10];
+    
+    struct Piece* whiteKnights[10];
+    struct Piece* blackKnights[10];
+    
+    struct Piece* whiteQueens[10];
+    struct Piece* blackQueens[10];
+    
+    struct Piece* whiteKing;
+    struct Piece* blackKing;
+    
+   
+    int i,j;
+
+    //Initialize board
+    for (i = 0; i < 8; i++)
+        for (j = 0; j < 8; j++)
+            board[i][j] = malloc(sizeof(struct Space));
+
+    //Initialize pawns
+    for (i = 0; i < 8; i++) {
+        whitePawns[i] = malloc(sizeof(struct Piece));
+        whitePawns[i]->piece_type = pawn;
+        whitePawns[i]->isWhite = true;
+        board[1][i]->piece = whitePawns[i];
+    
+        blackPawns[i] = malloc(sizeof(struct Piece));
+        blackPawns[i]->piece_type = pawn;
+        blackPawns[i]->isWhite = false;
+        board[6][i]->piece = blackPawns[i];
+    } 
+
     printBoard(board);
     
     return 0;
